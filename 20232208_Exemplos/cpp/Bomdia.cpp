b1580eafb9ff6399b6b14dbdc7d0f74cb3200771
Murilo Novais <muhnovais@gmail.com>
2023-08-22 17:11:58 -0300

    Primeiros Commits

---
+++ 20232208_Exemplos/cpp/Bomdia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    string Nome;
+    cout<< "Digite seu nome: ";
+    cin >> Nome;
+    cout <<"Bom dia, " <<Nome<< ". Tenha um bom curso!";
+    cout << endl;
+    return 0;
+}
