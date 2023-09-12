201613399b1af99f2ec254084f2aa43e1e66b654
Murilo Novais <muhnovais@gmail.com>
2023-09-12 13:55:15 -0300

    segundo commit

---
+++ 20232208_Exemplos/cpp/Atividade29/Atividadeaula13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Passagem {
+    float valor;
+    float data_hora;
+    Passageiro passageiro;
+    int assento;
+    int viagem,
+
+};
+struct Passageiro {
+    string nome;
+    string cpf;
+    string idade;
+
+};
+struct data_hora{
+    int dia;
+    int mes;
+    int ano;
+    int hora;
+    int minutod;
+};
+{
+    /* data */
+};
+
+int main() {
+    return 0;
+
+}
